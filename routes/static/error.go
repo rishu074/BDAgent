0720456fffb7769a144f82ae8b6687c8c276e11f
NotRoyadma <82922092+NotRoyadma@users.noreply.github.com>
2022-10-29 14:14:18 +0530

    404 templates

--- routes/static/error.go
+++ routes/static/error.go
@@ -1,9 +1,21 @@
 package static
 
 import (
+	"html/template"
 	"net/http"
 )
 
+type ErrorPageData struct {
+	Code    int
+	Message string
+}
+
 func ErrorRouteHandler(w http.ResponseWriter, r *http.Request, msg string, code int) {
-	http.Error(w, msg, code)
+	pageTemplate := template.Must(template.ParseFiles("./public/error.html"))
+
+	w.WriteHeader(http.StatusNotFound)
+	pageTemplate.Execute(w, ErrorPageData{
+		Code:    code,
+		Message: msg,
+	})
 }
